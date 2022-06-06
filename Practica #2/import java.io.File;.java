c1742a2326bed039fedcb91c77c647efe1c176c6
salcedios <106259687+salcedios@users.noreply.github.com>
2022-06-06 15:31:32 -0500

    Add files via upload

---
+++ Practica #2/import java.io.File;.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+public class palabra4 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = null;
+        String linea;
+        int numeroDeLinea = 1;
+        boolean contiene = false;
+
+        JFileChooser archivo = new JFileChooser();
+        archivo.showOpenDialog(archivo);
+
+        String palabra = "Numero";
+
+        try {
+
+            String ruta = archivo.getSelectedFile().getAbsolutePath();
+            File fichero = new File(ruta);
+            entrada = new Scanner(fichero);
+            System.out.println("Archivo: " + fichero.getName());
+            System.out.println("Texto a buscar: " + Numero);
+            while (entrada.hasNext()) { 
+                linea = entrada.nextLine();
+                if (linea.contains(Numero)) {       
+                    System.out.println("Linea " + numeroDeLinea + ": " + linea);
+                    contiene = true;
+                }
+                numeroDeLinea++;
+            }
+            if(!contiene){
+
+                System.out.println(Numero + " no se ha encontrado en el archivo");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (NullPointerException e) {
+            System.out.println(e.toString() + "No ha seleccionado ningún archivo");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            if (entrada != null) {
+                entrada.close();
+            }
+        }
+    }
+}
